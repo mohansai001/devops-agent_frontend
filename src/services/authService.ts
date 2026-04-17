d448ff2d2028cd03d51955f24c929ca8f27d5979
mohansai001 <mohan.ande@valuemomentum.com>
2026-04-17 15:08:14 +0530

    commit chnages

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -6,7 +6,7 @@ export interface CurrentUser {
 }
 
 export const startGithubLogin = (): void => {
-  window.location.href = 'https://devopsagent-backend-aegmehh9gcetepbf.eastus-01.azurewebsites.net/api/auth/github';
+  window.location.href = 'https://devops-backend-6c9x.onrender.com/api/auth/github';
 };
 
 export const getCurrentUser = async (): Promise<CurrentUser> => {
